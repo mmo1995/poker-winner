928b05f81f7e3864a5a57360572e33062a8416ff
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 00:42:09 +0100

    test(WinnerEvaluator): add test for StraightFlush vs Flush

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -90,4 +90,32 @@ public class WinnerEvaluatorTests
         //Assert
         Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
     }
+    
+    [Test]
+    public void Test_GetWinner_StraightFlush_vs_Flush()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator());
+        var cardHandStraightFlush = new CardHand([
+                new Card(CardSuit.C, CardValue.Five),
+                new Card(CardSuit.C, CardValue.Six),
+                new Card(CardSuit.C, CardValue.Seven),
+                new Card(CardSuit.C, CardValue.Eight),
+                new Card(CardSuit.C, CardValue.Nine)
+            ]
+        );
+        var cardHandFlush = new CardHand([
+            new Card(CardSuit.S, CardValue.Two),
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.S, CardValue.Nine),
+            new Card(CardSuit.S, CardValue.Nine)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandStraightFlush, cardHandFlush);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
+    }
 }
