e433a67a675e4709897ed55065e3130dc6750b55
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 00:39:23 +0100

    test(WinnerEvaluator): add test for StraightFlush vs FullHouse

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -62,4 +62,32 @@ public class WinnerEvaluatorTests
         //Assert
         Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
     }
+    
+    [Test]
+    public void Test_GetWinner_StraightFlush_vs_FullHouse()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator());
+        var cardHandStraightFlush = new CardHand([
+                new Card(CardSuit.C, CardValue.Five),
+                new Card(CardSuit.C, CardValue.Six),
+                new Card(CardSuit.C, CardValue.Seven),
+                new Card(CardSuit.C, CardValue.Eight),
+                new Card(CardSuit.C, CardValue.Nine)
+            ]
+        );
+        var cardHandFullHouse = new CardHand([
+            new Card(CardSuit.C, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Five),
+            new Card(CardSuit.H, CardValue.Five),
+            new Card(CardSuit.S, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Nine)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandStraightFlush, cardHandFullHouse);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
+    }
 }
