966682129df9773e885408eaabcc4a3a0875205c
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 00:35:48 +0100

    feat(core): implement winner evaluation for different ranks

--- PokerWinner/src/PokerWinnerEvaluator.CLI/Application/WinnerEvaluator.cs
+++ PokerWinner/src/PokerWinnerEvaluator.CLI/Application/WinnerEvaluator.cs
@@ -3,10 +3,15 @@ using PokerWinnerEvaluator.CLI.Domain;
 
 namespace PokerWinnerEvaluator.CLI.Application;
 
-public class WinnerEvaluator: IWinnerEvaluator
+public class WinnerEvaluator(IRankCalculator rankCalculator): IWinnerEvaluator
 {
     public CardHand GetWinner(CardHand hand1, CardHand hand2)
     {
-        throw new NotImplementedException();
+        var (hand1Rank, hand1Values) = rankCalculator.CalculateRank(hand1);
+        var (hand2Rank, hand2Values) = rankCalculator.CalculateRank(hand2);
+
+        if (hand1Rank != hand2Rank)
+            return hand1Rank > hand2Rank ? hand1 : hand2;
+        return null;
     }
 }
