d672c02191e65b8698c728d72ab7de7685be42ac
Mohammad Mohammad <mmohammad@mail.de>
2026-03-17 23:12:38 +0100

    feat(core): implement StraightFlush calculation

--- PokerWinner/src/PokerWinnerEvaluator.CLI/Application/RankCalculator.cs
+++ PokerWinner/src/PokerWinnerEvaluator.CLI/Application/RankCalculator.cs
@@ -7,6 +7,21 @@ public class RankCalculator: IRankCalculator
 {
     public (HandRank Rank, List<CardValue> Values) CalculateRank(CardHand hand)
     {
-        throw new NotImplementedException();
+        var sortedValues = hand.Cards.Select(card => card.Value).OrderByDescending(v => v).ToList();
+        var suitCount = hand.Cards.Select(card => card.Suit).Distinct().Count();
+        
+        if(IsStraightRank(sortedValues) && suitCount == 1)
+            return (HandRank.StraightFlush, sortedValues);
+        return (HandRank.HighCard, sortedValues);
+    }
+
+    private static bool IsStraightRank(List<CardValue> sortedValues)
+    {
+        for (var i = 0; i < sortedValues.Count - 1; i++)
+        {
+            if (sortedValues[i] - sortedValues[i + 1] != 1)
+                return false;
+        }
+        return true;
     }
 }
