c01444b781c042e5ffba8694ccbfd711b7b7d837
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 00:50:37 +0100

    test(WinnerEvaluator): add (failing) test for StraightFlush vs StraightFlush

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -8,31 +8,32 @@ public class WinnerEvaluatorTests
     private WinnerEvaluator? _winnerEvaluator;
     
     [Test]
-    public void Test_GetWinner_StraightFlush_vs_HighCard()
+    public void Test_GetWinner_StraightFlush_vs_StraightFlush()
     {
-     //Arrange
-     _winnerEvaluator = new WinnerEvaluator(new RankCalculator());
-     var cardHandStraightFlush = new CardHand([
-             new Card(CardSuit.C, CardValue.Five),
-             new Card(CardSuit.C, CardValue.Six),
-             new Card(CardSuit.C, CardValue.Seven),
-             new Card(CardSuit.C, CardValue.Eight),
-             new Card(CardSuit.C, CardValue.Nine)
-         ]
-     );
-     var cardHandHighCard = new CardHand([
-         new Card(CardSuit.C, CardValue.Five),
-         new Card(CardSuit.D, CardValue.Q),
-         new Card(CardSuit.H, CardValue.Three),
-         new Card(CardSuit.S, CardValue.Seven),
-         new Card(CardSuit.C, CardValue.Two)
-     ]);
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator());
+        var cardHandStraightFlushLowValue = new CardHand([
+                new Card(CardSuit.C, CardValue.Five),
+                new Card(CardSuit.C, CardValue.Six),
+                new Card(CardSuit.C, CardValue.Seven),
+                new Card(CardSuit.C, CardValue.Eight),
+                new Card(CardSuit.C, CardValue.Nine)
+            ]
+        );
+        var cardHandStraightFlushHighValue = new CardHand([
+                new Card(CardSuit.C, CardValue.Eight),
+                new Card(CardSuit.C, CardValue.Nine),
+                new Card(CardSuit.C, CardValue.Ten),
+                new Card(CardSuit.C, CardValue.J),
+                new Card(CardSuit.C, CardValue.Q)
+            ]
+        );
      
-     //Act
-     var winningCardHand = _winnerEvaluator.GetWinner(cardHandStraightFlush, cardHandHighCard);
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandStraightFlushLowValue, cardHandStraightFlushHighValue);
 
-     //Assert
-     Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlushHighValue));
     }
     
     [Test]
@@ -225,6 +226,34 @@ public class WinnerEvaluatorTests
      
         //Act
         var winningCardHand = _winnerEvaluator.GetWinner(cardHandStraightFlush, cardHandPair);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
+    }
+    
+    [Test]
+    public void Test_GetWinner_StraightFlush_vs_HighCard()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator());
+        var cardHandStraightFlush = new CardHand([
+                new Card(CardSuit.C, CardValue.Five),
+                new Card(CardSuit.C, CardValue.Six),
+                new Card(CardSuit.C, CardValue.Seven),
+                new Card(CardSuit.C, CardValue.Eight),
+                new Card(CardSuit.C, CardValue.Nine)
+            ]
+        );
+        var cardHandHighCard = new CardHand([
+            new Card(CardSuit.C, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Q),
+            new Card(CardSuit.H, CardValue.Three),
+            new Card(CardSuit.S, CardValue.Seven),
+            new Card(CardSuit.C, CardValue.Two)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandStraightFlush, cardHandHighCard);
 
         //Assert
         Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
