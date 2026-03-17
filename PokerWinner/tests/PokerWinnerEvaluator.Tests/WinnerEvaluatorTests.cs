177042792e54d38e79b20db9c535bb839f6f13cf
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 00:38:11 +0100

    test(WinnerEvaluator): add test for StraightFlush vs FourOfAKind

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -34,4 +34,32 @@ public class WinnerEvaluatorTests
      //Assert
      Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
     }
+    
+    [Test]
+    public void Test_GetWinner_StraightFlush_vs_FourOfAKind()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator());
+        var cardHandStraightFlush = new CardHand([
+                new Card(CardSuit.C, CardValue.Five),
+                new Card(CardSuit.C, CardValue.Six),
+                new Card(CardSuit.C, CardValue.Seven),
+                new Card(CardSuit.C, CardValue.Eight),
+                new Card(CardSuit.C, CardValue.Nine)
+            ]
+        );
+        var cardHandFourOfAKind = new CardHand([
+            new Card(CardSuit.C, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Five),
+            new Card(CardSuit.H, CardValue.Five),
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.C, CardValue.Nine)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandStraightFlush, cardHandFourOfAKind);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandStraightFlush));
+    }
 }
