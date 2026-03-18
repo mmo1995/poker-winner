a8e4c1a845be85cc8c4b9aaca094f83a85168a21
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 21:08:37 +0100

    test(WinnerEvaluator): add (failing) tests for HighCard vs HighCard

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -1340,4 +1340,37 @@ public class WinnerEvaluatorTests
     }
 
     #endregion Pair
+
+    #region HighCard
+
+    [Test]
+    public void Test_GetWinner_HighCard_vs_HighCard()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator(), new CardHandPairValidator());
+        
+        var cardHandHighCardHigh = new CardHand([
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.D, CardValue.A),
+            new Card(CardSuit.S, CardValue.Three),
+            new Card(CardSuit.H, CardValue.Eight),
+            new Card(CardSuit.S, CardValue.Two)
+        ]);
+        
+        var cardHandHighCardLow = new CardHand([
+            new Card(CardSuit.S, CardValue.Four),
+            new Card(CardSuit.D, CardValue.J),
+            new Card(CardSuit.S, CardValue.Six),
+            new Card(CardSuit.H, CardValue.Seven),
+            new Card(CardSuit.H, CardValue.Two)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandHighCardHigh, cardHandHighCardLow);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandHighCardHigh));
+    }
+
+    #endregion
 }
