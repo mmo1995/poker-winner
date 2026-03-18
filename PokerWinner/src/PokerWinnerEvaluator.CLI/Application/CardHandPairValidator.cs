862f1450b800dcab9e3391a7c3299a75a945b60d
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 19:35:52 +0100

    feat(core): implement Validate function of CardHandPairValidator

--- PokerWinner/src/PokerWinnerEvaluator.CLI/Application/CardHandPairValidator.cs
+++ PokerWinner/src/PokerWinnerEvaluator.CLI/Application/CardHandPairValidator.cs
@@ -1,5 +1,6 @@
 using PokerWinnerEvaluator.CLI.Contracts;
 using PokerWinnerEvaluator.CLI.Domain;
+using PokerWinnerEvaluator.CLI.Exceptions;
 
 namespace PokerWinnerEvaluator.CLI.Application;
 
@@ -7,6 +8,12 @@ public class CardHandPairValidator: ICardHandPairValidator
 {
     public void Validate(CardHand hand1, CardHand hand2)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(hand1);
+        ArgumentNullException.ThrowIfNull(hand2);
+
+        var allCards = hand1.Cards.Concat(hand2.Cards).ToList();
+
+        if (allCards.Count != allCards.Distinct().Count())
+            throw new InvalidCardHandPairException();
     }
 }
