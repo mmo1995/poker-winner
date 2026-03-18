a0f54f3c7d8f8951e991005e69a2593b109c1353
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 20:54:12 +0100

    test(WinnerEvaluator): add (failing) tests for TwoPairs vs TwoPairs

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -1104,6 +1104,93 @@ public class WinnerEvaluatorTests
     #region TwoPairs
 
     [Test]
+    public void Test_GetWinner_TwoPairs_vs_TwoPairs_different_high_pair()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator(), new CardHandPairValidator());
+        
+        var cardHandTwoPairsHigh = new CardHand([
+            new Card(CardSuit.H, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Five),
+            new Card(CardSuit.H, CardValue.Nine),
+            new Card(CardSuit.S, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Two)
+        ]);
+        
+        var cardHandTwoPairsLow = new CardHand([
+            new Card(CardSuit.H, CardValue.Four),
+            new Card(CardSuit.D, CardValue.Four),
+            new Card(CardSuit.H, CardValue.Eight),
+            new Card(CardSuit.S, CardValue.Eight),
+            new Card(CardSuit.C, CardValue.Three)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandTwoPairsHigh, cardHandTwoPairsLow);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandTwoPairsHigh));
+    }
+    
+    [Test]
+    public void Test_GetWinner_TwoPairs_vs_TwoPairs_same_high_pair_different_low_pair()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator(), new CardHandPairValidator());
+        
+        var cardHandTwoPairsHigh = new CardHand([
+            new Card(CardSuit.H, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Five),
+            new Card(CardSuit.H, CardValue.Nine),
+            new Card(CardSuit.S, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Two)
+        ]);
+        
+        var cardHandTwoPairsLow = new CardHand([
+            new Card(CardSuit.H, CardValue.Four),
+            new Card(CardSuit.D, CardValue.Four),
+            new Card(CardSuit.D, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Three)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandTwoPairsHigh, cardHandTwoPairsLow);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandTwoPairsHigh));
+    }
+    
+    [Test]
+    public void Test_GetWinner_TwoPairs_vs_TwoPairs_same_high_pair_same_low_pair()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator(), new CardHandPairValidator());
+        
+        var cardHandTwoPairsHigh = new CardHand([
+            new Card(CardSuit.H, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Five),
+            new Card(CardSuit.H, CardValue.Nine),
+            new Card(CardSuit.S, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.A)
+        ]);
+        
+        var cardHandTwoPairsLow = new CardHand([
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.C, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Three)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandTwoPairsHigh, cardHandTwoPairsLow);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandTwoPairsHigh));
+    }
+    
+    [Test]
     public void Test_GetWinner_TwoPairs_vs_Pair()
     {
         //Arrange
