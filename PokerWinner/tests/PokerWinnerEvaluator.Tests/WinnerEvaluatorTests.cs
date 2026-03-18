7826ee309093e6f6aa665e1f296951369be19bba
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 20:43:47 +0100

    test(WinnerEvaluator): add test for TwoPairs vs HighCard

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -1131,6 +1131,35 @@ public class WinnerEvaluatorTests
         //Assert
         Assert.That(winningCardHand, Is.EqualTo(cardHandTwoPairs));
     }
+    
+    [Test]
+    public void Test_GetWinner_TwoPairs_vs_HighCard()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator(), new CardHandPairValidator());
+        
+        var cardHandTwoPairs = new CardHand([
+            new Card(CardSuit.H, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Five),
+            new Card(CardSuit.H, CardValue.Nine),
+            new Card(CardSuit.S, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Two)
+        ]);
+        
+        var cardHandHighCard = new CardHand([
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Q),
+            new Card(CardSuit.S, CardValue.Three),
+            new Card(CardSuit.H, CardValue.Eight),
+            new Card(CardSuit.S, CardValue.Two)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandHighCard, cardHandTwoPairs);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandTwoPairs));
+    }
 
     #endregion TwoPairs
 }
