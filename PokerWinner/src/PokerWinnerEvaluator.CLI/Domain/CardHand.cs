1eff16bd20371dddbd1e933363b1e71f9aff1a76
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 18:24:27 +0100

    test(CardHand): add (failing) test for validating unique cards

--- PokerWinner/src/PokerWinnerEvaluator.CLI/Domain/CardHand.cs
+++ PokerWinner/src/PokerWinnerEvaluator.CLI/Domain/CardHand.cs
@@ -2,7 +2,12 @@ namespace PokerWinnerEvaluator.CLI.Domain;
 
 public class CardHand(IEnumerable<Card> cards) 
 {
-    public IEnumerable<Card> Cards { get; } = cards;
+    public IEnumerable<Card> Cards { get; } = Validate(cards);
     
     public override string ToString() => $"CardHand: {string.Join(", ", Cards.Select(c => c.ToString()))}";
+    
+    private static List<Card> Validate(IEnumerable<Card> cards)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/CardHandTests.cs
@@ -0,0 +1,23 @@
+using PokerWinnerEvaluator.CLI.Domain;
+
+namespace PokerWinnerEvaluator.Tests;
+
+public class CardHandTests
+{
+    private CardHand? _cardHand;
+    [Test]
+    public void Test_CardHand_Throws_Exception_When_Cards_Are_Not_Unique()
+    {
+        //Arrange & Act
+        _cardHand = new CardHand([
+                new Card(CardSuit.C, CardValue.Five),
+                new Card(CardSuit.C, CardValue.Five),
+                new Card(CardSuit.C, CardValue.Seven),
+                new Card(CardSuit.C, CardValue.Eight),
+                new Card(CardSuit.C, CardValue.Nine)
+            ]
+        );
+
+        //Assert
+    }
+}
