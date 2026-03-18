75fbddfc0681988b753e1968aebdfb324740dd4e
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 20:37:48 +0100

    test(WinnerEvaluator): add test for ThreeOfAKind vs HighCard

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -1040,6 +1040,35 @@ public class WinnerEvaluatorTests
         //Assert
         Assert.That(winningCardHand, Is.EqualTo(cardHandThreeOfAKind));
     }
+    
+    [Test]
+    public void Test_GetWinner_ThreeOfAKind_vs_HighCard()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator(), new CardHandPairValidator());
+        
+        var cardHandThreeOfAKind = new CardHand([
+            new Card(CardSuit.C, CardValue.Four),
+            new Card(CardSuit.D, CardValue.Four),
+            new Card(CardSuit.H, CardValue.Three),
+            new Card(CardSuit.H, CardValue.Two),
+            new Card(CardSuit.S, CardValue.Four)
+        ]);
+        
+        var cardHandHighCard = new CardHand([
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Q),
+            new Card(CardSuit.S, CardValue.Three),
+            new Card(CardSuit.H, CardValue.Eight),
+            new Card(CardSuit.C, CardValue.Two)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandThreeOfAKind, cardHandHighCard);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandThreeOfAKind));
+    }
 
     #endregion ThreeOfAKind
 }
