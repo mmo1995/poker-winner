b94e9b59bd9dbac5a3928f2ef30a05485722a8e9
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 01:38:55 +0100

    test(WinnerEvaluator): add test for FullHouse vs Pair

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/WinnerEvaluatorTests.cs
@@ -619,6 +619,33 @@ public class WinnerEvaluatorTests
         //Assert
         Assert.That(winningCardHand, Is.EqualTo(cardHandFullHouse));
     }
+    
+    [Test]
+    public void Test_GetWinner_FullHouse_vs_Pair()
+    {
+        //Arrange
+        _winnerEvaluator = new WinnerEvaluator(new RankCalculator());
+        var cardHandFullHouse = new CardHand([
+            new Card(CardSuit.C, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Five),
+            new Card(CardSuit.H, CardValue.Five),
+            new Card(CardSuit.S, CardValue.Nine),
+            new Card(CardSuit.C, CardValue.Nine)
+        ]);
+        var cardHandPair = new CardHand([
+            new Card(CardSuit.C, CardValue.Five),
+            new Card(CardSuit.D, CardValue.Q),
+            new Card(CardSuit.H, CardValue.Three),
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.C, CardValue.Two)
+        ]);
+     
+        //Act
+        var winningCardHand = _winnerEvaluator.GetWinner(cardHandFullHouse, cardHandPair);
+
+        //Assert
+        Assert.That(winningCardHand, Is.EqualTo(cardHandFullHouse));
+    }
 
     #endregion FullHouse
 }
