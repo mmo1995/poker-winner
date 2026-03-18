28f3cc698729d8a409bd1ae47f2e41579d4a1d79
Mohammad Mohammad <mmohammad@mail.de>
2026-03-18 19:37:37 +0100

    test(CardHandPairValidator): add test for validating unique cards in pair - success case

--- PokerWinner/tests/PokerWinnerEvaluator.Tests/CardHandPairValidatorTests.cs
+++ PokerWinner/tests/PokerWinnerEvaluator.Tests/CardHandPairValidatorTests.cs
@@ -31,4 +31,29 @@ public class CardHandPairValidatorTests
         //Act & Assert
         Assert.Throws<InvalidCardHandPairException>(() => _cardHandPairValidator.Validate(cardHand1, cardHand2));
     }
+    
+    [Test]
+    public void Test_Does_Not_Throw_Exception_When_Cards_Are_Unique_In_Pair()
+    {
+        //Arrange
+        _cardHandPairValidator = new CardHandPairValidator();
+        var cardHand1 = new CardHand([
+            new Card(CardSuit.C, CardValue.Five),
+            new Card(CardSuit.C, CardValue.Four),
+            new Card(CardSuit.C, CardValue.Seven),
+            new Card(CardSuit.C, CardValue.Eight),
+            new Card(CardSuit.C, CardValue.Nine)
+        ]);
+
+        var cardHand2 = new CardHand([
+            new Card(CardSuit.S, CardValue.Five),
+            new Card(CardSuit.S, CardValue.Four),
+            new Card(CardSuit.S, CardValue.Seven),
+            new Card(CardSuit.S, CardValue.Eight),
+            new Card(CardSuit.S, CardValue.Nine)
+        ]);
+
+        //Act & Assert
+        Assert.DoesNotThrow(() => _cardHandPairValidator.Validate(cardHand1, cardHand2));
+    }
 }
